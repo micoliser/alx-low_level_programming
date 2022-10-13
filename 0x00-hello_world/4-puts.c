bcdceece13eb289c722e77450e17acfa832ce6e0
micoliser <micoliser@gmail.com>
2022-10-13 08:19:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-       	char my_string[] = "Programming is like building a multilingual puzzle";
+	char my_string[] = "\" Programming is like building a multilingual puzzle";
+
 	puts(my_string);
 
-	return(0);
+	return (0);
 }
