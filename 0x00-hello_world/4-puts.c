11ef42cffaf741f784e4101af5b66ef6baa8e9da
micoliser <micoliser@gmail.com>
2022-10-13 08:28:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints out a string
+ * Return: returns 0
+ */
 int main(void)
 {
-	char my_string[] = "\" Programming is like building a multilingual puzzle";
+	char my_string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(my_string);
 
