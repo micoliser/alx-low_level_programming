ac8995e7eba13bd7b626d1f4cb3c4a2f5931108e
micoliser <micoliser@gmail.com>
2022-10-14 08:03:55 -0700

    2-print_alphaber.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all th alphabets in upercase
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}	
+	putchar('\n');
+
+	return (0);
+}
