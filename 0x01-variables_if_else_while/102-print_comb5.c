cd640a1bfe1e7d5f6b2043adf5185b4c72c2efe6
micoliser <micoliser@gmail.com>
2022-10-14 11:17:01 -0700

    print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints out two 2digit numbers
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if (a + b < c + d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+
+if (a == '9' && b == '8' && b == '9' && d == '9')
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
