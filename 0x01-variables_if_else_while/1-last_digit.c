7ab2b3551d598eaf175d22cdc90e98b98a3c87b1
micoliser <micoliser@gmail.com>
2022-10-14 07:35:28 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int n;
+	int lst_digt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	int lst_digt = n % 10;
+	lst_digt = n % 2;
 
 	if (lst_digt > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lst_digt);
