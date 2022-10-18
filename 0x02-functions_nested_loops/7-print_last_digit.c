a7587f9cd2b3e36c25e87169cdabfe6b457e73f3
micoliser <micoliser@gmail.com>
2022-10-18 08:12:45 -0700

    7_print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: input number
+ *
+ * Return: returns the value of the last digit
+ */
+int print_last_digit(int c)
+{
+	int last_digit = c % 10;
+
+	return (last_digit);
+}
