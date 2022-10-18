aba97a619552c225a008899f808465c48f9f625c
micoliser <micoliser@gmail.com>
2022-10-18 09:25:46 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: returns nothing
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			k = j * i
+			_putchar(k + 48);
+
+			if (j != '9')
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
