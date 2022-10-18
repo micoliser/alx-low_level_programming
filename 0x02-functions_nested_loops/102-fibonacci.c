3bef583c63aa0ea0c55673fd3370414392b75915
micoliser <micoliser@gmail.com>
2022-10-18 12:09:54 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_fibonacci(int);
+
+/**
+ * main - calls print_fibonacci with 50
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n = 50;
+
+	print_fibonacci(50);
+
+	return (0);
+}
+
+/**
+ * print_fibonacci - prints out the fibonacci sequence of numbers
+ * @i: the input number
+ *
+ * Return: returns nothing
+ */
+void print_fibonacci(int i)
+{
+	int a, b;
+	int l = 2;
+	int p = 1;
+
+	printf("%d, %d, ", p, l);
+
+	for (a = 3; a <= i; a++)
+	{
+		b = p + l;
+
+		printf("%d", b);
+
+		if (a != i)
+			printf(", ");
+
+		p = l;
+		l = b;
+	}
+	printf("\n");
+}
