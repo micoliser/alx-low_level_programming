a4225cb94395b264e5a7c008d949f8cc1936258e
micoliser <micoliser@gmail.com>
2022-10-18 07:03:30 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * _islower - checks for a lowercase character
  * @c: the character to check
  *
- * Return - returns 1 if true and 0 otherwise
+ * Return: returns 1 if true and 0 otherwise
  */
 
 int _islower(int c)
@@ -13,8 +13,8 @@ int _islower(int c)
 	int i = islower(c);
 
 	if (i == 0)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 
 }
