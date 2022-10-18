d9a6d745c326667c8730f0748e6f7cbb47aca8b7
micoliser <micoliser@gmail.com>
2022-10-18 09:00:04 -0700

    *-jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 int i, j, k, l;
 
-for (i = '0'; i <= 2; i++)
+for (i = '0'; i <= '2'; i++)
 {
-	for (j = 0; j <= '9'; j++)
+	for (j = '0'; j <= '9'; j++)
 	{
 		for (k = '0'; k <= '5'; k++)
 		{
 			for (l = '0'; l <= '9'; l++)
 			{
-				if (i == '2' && j == '4)
+				if (i == '2' && j == '4')
 					break;
 				_putchar(i);
 				_putchar(j);
