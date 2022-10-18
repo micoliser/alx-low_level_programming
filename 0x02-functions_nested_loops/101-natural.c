2a0d1036f52a93089d1d248c4859140e0922cdf8
micoliser <micoliser@gmail.com>
2022-10-18 11:42:56 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void print_numbers(int);
+
+/**
+ * main - calls print_numbers function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n = 1024;
+
+	print_numbers(n);
+
+	return (0);
+}
+
+/**
+ * print_numbers - prints the sum of multiples of 5 and 3
+ * @i: number to be checked
+ *
+ * Return: returns nothing
+ */
+void print_numbers(int i)
+{
+	int a, sum;
+
+	for (a = 0; a < i; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+			sum += a;
+	}
+
+	printf("%d", sum);
+}
