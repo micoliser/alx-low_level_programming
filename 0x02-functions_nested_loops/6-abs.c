969cfa0ce4e83bec45c680ecec3d8d0c4e0b672d
micoliser <micoliser@gmail.com>
2022-10-18 08:02:23 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,14 @@
 
 /**
  * _abs - computes the absolute value of a number
+ * @c: number to be checked
  *
  * Return: returns the absolute value of the number given
  */
 int _abs(int c)
 {
-	int value = abs(c);
-
-	return (value);
+	if (c > 0)
+		return (c);
+	else
+		return (-c);
 }
