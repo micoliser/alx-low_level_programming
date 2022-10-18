feb451ddfcb449678d8aa366fde55719d7ed1adc
micoliser <micoliser@gmail.com>
2022-10-18 09:30:27 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,14 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = j * i;
 			_putchar(k + 48);
 
-			if (j != '9')
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
