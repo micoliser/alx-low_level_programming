1cecc852ff12d60680126ad8e1fd45bd39d26431
micoliser <micoliser@gmail.com>
2022-10-18 10:43:54 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,33 +11,19 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d", n);
-
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
 			n++;
 		}
-		_putchar('\n');
 	}
 	else
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d", n);
-
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 			n--;
 		}
-		_putchar('\n');
 	}
+	printf("%d\n", n);
 }
