cbd9f53d760ecfdb18e1399fdb66c18558f018f9
micoliser <micoliser@gmail.com>
2022-10-18 08:58:24 -0700

    *-jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints out every minute of the day
+ *
+ * Return: returns nothing
+ */
+void jack_bauer(void)
+{
+int i, j, k, l;
+
+for (i = '0'; i <= 2; i++)
+{
+	for (j = 0; j <= '9'; j++)
+	{
+		for (k = '0'; k <= '5'; k++)
+		{
+			for (l = '0'; l <= '9'; l++)
+			{
+				if (i == '2' && j == '4)
+					break;
+				_putchar(i);
+				_putchar(j);
+				_putchar(58);
+				_putchar(k);
+				_putchar(l);
+				_putchar('\n');
+			}
+		}
+	}
+}
