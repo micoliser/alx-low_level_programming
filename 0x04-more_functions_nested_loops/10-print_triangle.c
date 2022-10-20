ca0bec7fc243b61d5c6765adf8c005579afc50fc
micoliser <micoliser@gmail.com>
2022-10-20 06:10:51 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - 1; j > i; j--)
+			{
+				_putchar(' ');
+			}
+
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);	
+
+			}
+			
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
