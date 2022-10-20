0dbb9bf935ae28b1c8b2821f9dada5c63638da03
micoliser <micoliser@gmail.com>
2022-10-20 12:02:22 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,12 @@
  * Return: void
  */
 void print_number(int n)
-{
-	unsigned int m;
-	
+{	
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		n = n * -1;
 	}
 	else if (n == 0)
 	{
@@ -22,10 +20,8 @@ void print_number(int n)
 		return;
 	}
 
-	m = n;
+	if (n / 10)
+		print_number(n / 10);
 
-	if (m / 10)
-		print_number(m / 10);
-
-	_putchar(m % 10 + 48);
+	_putchar(n % 10 + 48);
 }
