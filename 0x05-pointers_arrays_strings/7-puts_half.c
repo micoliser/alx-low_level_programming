d3f8607e4305db72f512fe71799160c5ab25a08c
micoliser <micoliser@gmail.com>
2022-10-24 07:52:58 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of a string
+ * @str: the pointer to the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, st, len;
+
+	len = strlen(str);
+
+	if (len % 2 != 0)
+	{
+		st = (len - 1) / 2;
+	}
+	else
+	{
+		st /= 2;
+	}
+
+	for (i = st; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}	
