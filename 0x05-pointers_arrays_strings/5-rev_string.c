6b6f5c99182477731aa0099632e7c07df3486cca
micoliser <micoliser@gmail.com>
2022-10-24 06:48:35 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the pointer to the  string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, len;
+	char newstr[];
+
+	len = strlen(s);
+	i = len - 1;
+	j = 0;
+
+	while (i >= len)
+	{
+		newstr[j] = str[i];
+
+		i--;
+		j++;
+	}
+
+	s = newstr;
+}
+
+
