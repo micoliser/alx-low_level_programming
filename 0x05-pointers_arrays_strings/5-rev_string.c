49ecdbd2eea12cb5e8eb908ebdc07391bec2b501
micoliser <micoliser@gmail.com>
2022-10-24 07:03:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,20 +9,5 @@
  */
 void rev_string(char *s)
 {
-	int i, j, len;
-	char newstr[20];
-
-	len = strlen(s);
-	i = len - 1;
-	j = 0;
-
-	while (i >= len)
-	{
-		newstr[j] = s[i];
-
-		i--;
-		j++;
-	}
-
-	s = newstr;
+	strrev(s);
 }
