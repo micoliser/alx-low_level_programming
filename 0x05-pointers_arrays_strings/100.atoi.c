94a104aa30ae65e7fc7060f0588bf3cfe7866800
micoliser <micoliser@gmail.com>
2022-10-24 08:54:02 -0700

    100.atoi.c

---
+++ 0x05-pointers_arrays_strings/100.atoi.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the pointer to the string
+ *
+ * Return: the integer
+ */
+int _atoi(char *s)
+{
+	int val = atoi(s);
+
+	return (val);
+}
