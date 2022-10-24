a1f6cc71c0e35eb86f93b9c30bb33147e70c73d7
micoliser <micoliser@gmail.com>
2022-10-24 05:51:53 -0700

    1-swaqp.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * swap_int - swaps the valuse of two integers
+ * @a: pointer of the first integer
+ * @b: pointer of the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
