ba69f0b63c5616f00d2579bdd11ac306a38d0d19
micoliser <micoliser@gmail.com>
2022-10-24 07:34:58 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every even character in a string
+ * @str: the pointer to the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i <= len - 1; i++)
+	{
+		if (str[1] % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
