6cde16c729a260361452f48bce2e472148fe6e63
micoliser <micoliser@gmail.com>
2022-10-26 10:19:12 -0700

    corrected 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/103_infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * infite add - add two numbere
+ * @n1: pointer to first number
+ * @n2: pointer to second number
+ * @r: pointer to where to store the result;
+ * @size_r: size of r
+ *
+ * Return: a pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, int size_r)
+{
+	r = n1 + n2;
+
+	
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,7 +9,7 @@ void reverse_array(int *a, int n)
 {
 	int i, c;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		c = a[i];
 		a[i] = a[n - i - 1];
