7df881cdc87026c8273d0f9d9699a16e7a17772d
micoliser <micoliser@gmail.com>
2022-10-26 14:53:01 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,7 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
 
-	if (len1 > size_r)
+	if (len1 + 1 >= size_r)
 		return (0);
 
 	j = len2 - 1;
