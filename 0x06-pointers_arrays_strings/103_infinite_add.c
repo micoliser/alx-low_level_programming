3f191e448161b615412a5bc923d948e24c11da01
micoliser <micoliser@gmail.com>
2022-10-26 14:39:21 -0700

    103_infinite_add.c

--- 0x06-pointers_arrays_strings/103_infinite_add.c
+++ 0x06-pointers_arrays_strings/103_infinite_add.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -9,8 +10,78 @@
  *
  * Return: a pointer to the result
  */
-char *infinite_add(char *n1, char *n2, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	r = n1 + n2;
+	int i, j, k, l, num;
+	int d [500];
+	int rem = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
 
-	
+	j = len2 - 1;
+	k = len1 - len2 - 1;
+
+	for (i = len1 - 1; i > k; i--)
+	{
+		while (j >= 0)
+		{
+			num = rem + (n1[i] - 48) + (n2[j] - 48);
+
+			if (num > 9)
+			{
+				num = num - 10;
+				rem = 1;
+			}
+			else
+			{
+				rem = 0;
+			}
+
+			d[i] = num;
+			j--;
+			break;
+		}
+	}
+
+	for (l = i; l >= 0; l--)
+	{
+		num = n1[l] - 48;
+
+		if (rem == 1 && l == i)
+			num = num + 1;
+
+		if (num > 9)
+		{
+			num = num - 10;
+			rem = 1;
+		}
+		else
+		{
+			rem = 0;
+		}
+		d[l] = num;
+	}
+
+	if (rem == 1)
+		d[0] += 10;
+
+	i = 0;
+	j = 0;
+
+	if (d[0] > 9)
+	{
+		r[0] = 49;
+		r[1] = (d[0] % 9) + 48;
+		j = 2;
+		i = 1;
+	}
+
+	while (i < len1)
+	{
+		r[j] = d[i] + 48;
+		i++;
+		j++;
+	}
+
+	return (r);
+}
