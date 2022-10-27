44ae1de29d7569345e9b1af516cda7687938cf91
micoliser <micoliser@gmail.com>
2022-10-27 16:28:25 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,39 +12,35 @@ void print_buffer(char *b, int size)
 {
 	int i, j, k;
 
-	if (size <= 0)
+	for (i = 0; i < size; i = i + 10)
 	{
+		printf("%08x:", i);
+
+		for (j = 0; k < 10; k++)
+		{
+			if (j + i >= size)
+				printf(" ");
+			else
+				printf("%02x", b[i + j]);
+			if (j % 2 != 0 && j != 0)
+				printf(" ");
+		}
+
+		for (k = 0; k < 10; k++)
+		{
+			if (k + i >= size)
+				break;
+			if (b[i + k] > 32 || b[i + k] < 127)
+				printf("%c", b[i + k]);
+			else
+				printf(".");
+		}
+		if (i >= size)
+			continue;
+
 		printf("\n");
 	}
-	else
-	{
-		for (i = 0; i < size; i = i + 10)
-		{
-			printf("%08x:", i);
-			
-			for (j = 0; k < 10; k++)
-			{
-				if (j + i >= size)
-					printf(" ");
-				else
-					printf("%02x", b[i + j]);
-				if (j % 2 != 0 && j != 0)
-					printf(" ");
-			}
-
-			for (k = 0; k < 10; k++)
-			{
-				if (k + i >= size)
-					break;
-				if (b[i + k] > 32 || b[i + k] < 127)
-					printf("%c", b[i + k]);
-				else
-					printf(".");
-			}
-			if (i >= size)
-				continue;
-
-			printf("\n");
-		}
-	}
+	
+	if (size <= 0)
+		printf("\n");
 }
