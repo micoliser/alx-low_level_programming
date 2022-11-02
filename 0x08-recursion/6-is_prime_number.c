be132becfd1b47a6c2f426abe3b1cdb82354a05b
micoliser <micoliser@gmail.com>
2022-11-02 05:25:19 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,13 @@ int check_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	if (n < 0)
+		n = n * -1;
+
+	if (n == 1)
+		return (0);
+	else
+		return (check_prime(n, 2));
 }
 
 /**
@@ -22,9 +28,6 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-	if (n < 0)
-		n = -n;
-
 	if (i > n / 2)
 		return (1);
 	else if (n % i == 0)
