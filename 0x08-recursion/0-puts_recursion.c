eb2dcd5151380cb6e262c9f9574e34efe49b9590
micoliser <micoliser@gmail.com>
2022-11-02 02:57:26 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	i = 0;
+
+	if (i == len)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(s[i]);
+	_puts_recursion(s[i + 1]);
+}
