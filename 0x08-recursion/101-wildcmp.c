7bd3739f4f9227c9842569ead3e25ca60fd2b04b
micoliser <micoliser@gmail.com>
2022-11-02 12:41:07 -0700

    101-wildcmp.c fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,10 +16,10 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 
 	if (s1[0] == s2[0])
-		return (wildcmp(&s1[1], &s1[1]);
+		return (wildcmp(&s1[1], &s1[1]));
 
 	if (s2[0] == '*')
-		return (wildcmp(&s1[0], &s2[1]) || wildcmp(&s1[1], &s2[0]);
+		return (wildcmp(&s1[0], &s2[1]) || wildcmp(&s1[1], &s2[0]));
 
 	return (0);
 }
