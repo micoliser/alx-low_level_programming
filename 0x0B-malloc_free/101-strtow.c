3375a2caf08ef74de60be8131fef19bb124b972b
micoliser <micoliser@gmail.com>
2022-11-07 06:56:10 -0800

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: a pointer to an array of strings or NULL if error
+ */
+char **strtow(char *str)
+{
+	char **a, prev;
+	int i, j, k, len, size, size2;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	len = strlen(str);
+	prev = ' ';
+	j = 0;
+	k = 0;
+	
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ' && prev == ' ')
+			size++;
+		prev = str[i];
+	}
+
+	a = malloc(size * sizeof(char *));
+
+	prev = 'k';
+	for (i = 0; i < size; i++)
+	{
+		while (j < len)
+		{
+			if (str[j] != ' ' && prev == ' ')
+			{
+				prev = 'k';
+				break;
+			}
+			if (str[j] != ' ')
+				size2++;
+			prev = str[j];
+			j++;
+		}
+		
+		a[i] = malloc(size2 * sizeof(char));
+		if (a[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		while (j < len)
+		{
+			if (str[j] != ' ' && prev == ' ')
+			{
+				prev = 'k';
+				break;
+			}
+			if (str[j] != ' ')
+			{
+				a[i][k] = str[j];
+				k++;
+			}
+			prev = str[j];
+			j++;
+			k++;
+		}
+		a[i][k] == '\0';
+
+		k = 0;
+	}
+	a[i] = NULL;
+
+	return (a);
+}
+
