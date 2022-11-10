9d29c3932eaa238e180d40101ab3447042fdfeea
micoliser <micoliser@gmail.com>
2022-11-10 05:43:38 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		len2 = strlen(s2);
 
-	s = malloc((len1 + len + 1) * sizeof(char));
+	s = malloc((len1 + n + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
