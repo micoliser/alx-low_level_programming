590da983034b90c047cf69636ec22811b3c8bc83
micoliser <micoliser@gmail.com>
2022-11-10 05:42:30 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,27 +12,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, len, len1, len2, len3;
+	unsigned int i, len1, len2, len3;
 
 	if (s1 == NULL)
 		len1 = 0;
 	else
 		len1 = strlen(s1);
+
 	if (s2 == NULL)
 		len2 = 0;
 	else
 		len2 = strlen(s2);
-	if (n >= len2)
-		len = len2;
-	else
-		len = n;
 
 	s = malloc((len1 + len + 1) * sizeof(char));
+
 	if (s == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
-	for (i = 0; i < len; i++)
+	for (i = 0; i < n; i++)
 		s[i + len1] = s2[i];
 	len3 = strlen(s);
 	s[len3] = '\0';
