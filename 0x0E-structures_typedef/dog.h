497b7b1c77af434ebaa4d2c2d68a3498b741cc9a
micoliser <micoliser@gmail.com>
2022-11-14 06:18:31 -0800

    dog.h 4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - a structure of dog
  * @name: name of dog
  * @age: age of dog
@@ -18,5 +23,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
