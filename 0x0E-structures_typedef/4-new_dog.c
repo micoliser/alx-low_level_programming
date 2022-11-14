0940ebb0bec60de0606a473f4a45fa511a5117dd
micoliser <micoliser@gmail.com>
2022-11-14 06:29:27 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,12 +4,15 @@
  * new_dog - creates a new dog structure
  * @name: name of the dog
  * @age: age of the dog
- * @owner
+ * @owner: owner of the dog
  *
  * Return: the new dog greated or null if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *dog_name = name;
+	char *dog_owner = owner;
+
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
@@ -21,5 +24,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->age = age;
 	dog->owner = owner;
 
-	return dog;
+	return (dog);
 }
