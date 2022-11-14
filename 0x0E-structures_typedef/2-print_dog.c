ec71bf6cae35ce1c5754e1290ca99750e3c5eb87
micoliser <micoliser@gmail.com>
2022-11-14 04:40:24 -0800

    added files dog.h 1-init_dog.c 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog
+ * @d: a struct of the dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (!= NULL)
+	{
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %.6f\n", d->age == NULL ? "(nil)" : d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this is a structure type of a dog containing its name
+ * age and owner
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif
