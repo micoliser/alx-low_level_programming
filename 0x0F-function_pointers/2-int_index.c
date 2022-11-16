389a7f4ec0907726de1f99b17fe566de0909ff25
micoliser <micoliser@gmail.com>
2022-11-16 03:46:25 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,6 +2,7 @@
 
 /**
  * int_index - searches for an integer
+ * @array: the array
  * @size: size of array
  * @cmp: pointer to the compare function
  *
@@ -14,10 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+
+	if (cmp && array)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
 				return (i);
+		}
+
 	}
 
 	return (-1);
