11a9fc269a70685179559937df86923d81ec9857
micoliser <micoliser@gmail.com>
2022-11-16 05:18:48 -0800

    fixed 3_main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,10 +18,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	opr = get_op_func(argv[2]);
