5ef3df5903b567801c7e59f14a332eb241c64014
micoliser <micoliser@gmail.com>
2022-11-29 02:45:43 -0800

    101-print_listint_safe.c 102-free_listint_safe.c 103-find_loop.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,18 +8,62 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *list = head;
-	size_t count = 0;
+	size_t num_nodes = 0;
+	listptr_t *hptr, *new, *add;
 
-	if (!head)
-		exit(98);
-
-	while (list)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)list, list->n);
-		count++;
-		list = list->next;
+		new = malloc(sizeof(listptr_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (num_nodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		num_nodes++;
 	}
 
-	return (count);
+	free_listp(&hptr);
+	return (num_nodes);
+}
+
+/**
+ * free_listp - frees a linked list
+ * @head: head of the list.
+ *
+ * Return: void
+ */
+void free_listp(listptr_t **head)
+{
+	listptr_t *temp;
+	listptr_t *present;
+
+	if (head != NULL)
+	{
+		present = *head;
+		while ((temp = present) != NULL)
+		{
+			present = present->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listptr_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listptr_s
+{
+	void *p;
+	struct listptr_s *next;
+} listptr_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -33,5 +46,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-
+size_t free_listint_safe(listint_t **);
+listint_t *find_listint_loop(listint_t *);
+void free_listint(listint_t *);
+void free_listint2(listint_t **);
 #endif
