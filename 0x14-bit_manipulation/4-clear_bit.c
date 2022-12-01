045a93520ed7ac9d17a67368e3f45555567452b5
micoliser <micoliser@gmail.com>
2022-12-01 05:36:08 -0800

    fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,13 +13,14 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 64)
 		return (-1);
+
 	if (*n == 0)
 		return (0);
 
 	for (i = 0; i < index; i++)
 		num *= 2;
 
-	*n = *n ^ num;
+	*n = *n & ~num;
 
 	return (1);
 }
