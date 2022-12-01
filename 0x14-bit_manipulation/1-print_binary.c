52ee8bd45a32dc4fa5b98d3d38c3b528daefdb5f
micoliser <micoliser@gmail.com>
2022-12-01 06:26:05 -0800

    fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,28 +8,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int num = 0, i = 1;
-
-	if (n >> 0 == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-
-	while (i * 2 <= n)
-		i *= 2;
-
-	while (i != 0)
-	{
-		if ((num + i > n) || num == n)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-			num += i;
-		}
-		i = i >> 1;
 	}
 }
