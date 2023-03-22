e13e52b5e4061d0023be4eb8bc67371c6b6d4b52
micoliser <micoliser@gmail.com>
2023-03-22 22:05:37 +0100

    last fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,23 +19,14 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
 
-        if (i == 0 or i == len(grid) - 1) and 1 in grid[i]:
-            return 0
-
         if 1 in grid[i]:
             perimeter += 2
 
         for j in range(len(grid[i])):
-            if (j == 0 or j == len(grid[i]) - 1) and grid[i][j] == 1:
-                return 0
-
-            try:
-                if grid[i][j] == 1 and grid[i - 1][j] == 0:
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0 or i == 0:
                     perimeter += 1
-
-                if grid[i][j] == 1 and grid[i + 1][j] == 0:
+                if grid[i + 1][j] == 0 or i == len(grid) - 1:
                     perimeter += 1
-            except IndexError:
-                pass
 
     return perimeter
